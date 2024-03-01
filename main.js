b0b3d9fbcce829aedbce3fd7f967e7857d07f4cb
MoeMoeAung-MMA17 <moe603958@gmail.com>
2024-03-01 15:44:10 +0630

    loading updated

--- main.js
+++ main.js
@@ -278,7 +278,7 @@ document.addEventListener("DOMContentLoaded", function() {
       document.getElementById("loaderContainer").classList.add("animate__fadeOut");
       setTimeout(() => {
         document.getElementById("loaderContainer").style.display = "none";
-      }, 5000); // Wait for animation to finish before hiding the loader
+      }, 10000); // Wait for animation to finish before hiding the loader
     }
 
     // Function to show the loader
